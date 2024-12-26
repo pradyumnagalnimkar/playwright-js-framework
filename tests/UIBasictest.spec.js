8fec8e3a67d60435f88366e605d9a789f24f4b87
Pradyumna Galnimkar <pradyumna.galnimkar@gmail.com>
2024-12-26 22:55:57 +0530

    automated invalid login test

--- tests/UIBasictest.spec.js
+++ tests/UIBasictest.spec.js
@@ -1,6 +1,6 @@
-const { test } = require("@playwright/test")
+const { test, expect } = require("@playwright/test")
 
-test('Browser fixture test', async ({ browser , baseURL }) => {
+test('Browser fixture test', async ({ browser }) => {
     /**
      * 1. Create new browser context or new browser instance using browser fixture
      * 2. Create new page for above created browser context
@@ -11,11 +11,20 @@ test('Browser fixture test', async ({ browser , baseURL }) => {
     console.log(await page.title())
 })
 
-test('Page fixture test', async ({ page , baseURL }) => {
+test('Page fixture test', async ({ page }) => {
     /**
      * 1. Use Page fixture directly for opening new browser instance
      * Note: Since no configOptions are specified we do not need to create browser context
      */
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/")
     console.log(await page.title())
+})
+
+test.only('Login page test', async ( {page} ) => {
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/")
+    await page.locator("#username").fill("rahulshettyacademy")
+    await page.locator("[name='password']").fill("Test123")
+    await page.locator("[type='submit']").click()
+    console.log(await page.locator("[style*='block']").textContent())
+    await expect(page.locator("[style*='block']")).toContainText("Incorrect username/password.")
 })
