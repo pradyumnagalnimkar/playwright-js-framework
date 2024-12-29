c3c20c2522739250e61e1c4a4206428488c4a6e5
Pradyumna Galnimkar <pradyumna.galnimkar@gmail.com>
2024-12-29 11:55:44 +0530

    test to handle multiple tabs

--- tests/handle_dialogs_frames.spec.js
+++ tests/handle_dialogs_frames.spec.js
@@ -8,7 +8,7 @@ test("Verify hidden elements", async({ page }) => {
     await expect(page.getByPlaceholder("Hide/Show Example")).toBeVisible()
 })
 
-test.only("Handle alert popups", async({ page }) => {
+test("Handle alert popups", async({ page }) => {
     await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
     await page.getByPlaceholder("Enter Your Name").fill("Alert handles")
     page.on('dialog', dialog => {
@@ -16,4 +16,13 @@ test.only("Handle alert popups", async({ page }) => {
         dialog.dismiss()
     })
     await page.locator("#confirmbtn").click()
+})
+
+test('handle multiple windows', async({ browser }) => {
+    const context = await browser.newContext()
+    const page = await context.newPage()
+    await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
+    const [newPage] = await Promise.all([context.waitForEvent('page'), page.locator("#opentab").click()])
+    await expect(newPage.getByText("Access all our Courses")).toBeVisible()
+    
 })
