42ab332ef5fadd561054c60f6e4c500efe9cbb88
Pradyumna Galnimkar <pradyumna.galnimkar@gmail.com>
2024-12-30 18:51:53 +0530

    api test using playwright

--- tests/place_order_api.spec.js
+++ tests/place_order_api.spec.js
@@ -5,6 +5,9 @@ const login_payload = {
     userPassword: "Test@12345678"
     }
 let token;
+const order_payload = {orders:[{country:"Cuba",productOrderedId:"6581ca979fd99c85e8ee7faf"}]}
+let order_id ;
+exports.order_id = order_id;
 
 test.beforeAll('Login to app via API before all', async() => {
     const api_context = await request.newContext()
@@ -12,12 +15,41 @@ test.beforeAll('Login to app via API before all', async() => {
     expect(login_response.ok()).toBeTruthy()
     const login_response_json = await login_response.json();
     token = login_response_json.token
-    console.log(token)
+
+    const order_reponse = await api_context.post("https://rahulshettyacademy.com/api/ecom/order/create-order", {
+        data: order_payload,
+        headers: {
+            "Authorization": token,
+            "Content-Type": "application/json"
+        },
+    })
+    const order_response_json = await order_reponse.json()
+    order_id = order_response_json.orders[0]
 })
 
-test.only('Login to app via API', async({ page }) => {
+test('Login to app via API', async({ page }) => {
     await page.addInitScript(value => {
         window.localStorage.setItem('token', value)
     }, token)
+
+    /**
+     * Place order API
+     */
     await page.goto("https://rahulshettyacademy.com/client")
+    await page.getByRole("listitem").getByRole("button", {name:"ORDERS"}).click()
+    await page.waitForLoadState('networkidle')
+
+    const orders = page.locator("tbody tr")
+    await orders.first().waitFor()
+    const number_of_orders = await orders.count()
+    for(let i=0;i<number_of_orders;i++){
+        if(order_id.includes(await orders.nth(i).locator('th').textContent())){
+            await orders.nth(i).locator('text=View').click()
+            break
+        }
+    }
+    await expect(page.getByText("Thank you for Shopping With Us")).toBeVisible()
+    await expect(page.locator(".email-title")).toContainText(" order summary ")
+    const actualOrderId = await page.locator(".-main").textContent()
+    expect(order_id.includes(actualOrderId)).toBeTruthy()
 })
