ce6f63ec43f88e9d93bf160f5f6cc23e785543ce
Pradyumna Galnimkar <pradyumna.galnimkar@gmail.com>
2025-01-02 18:59:35 +0530

    Revert "implemented common page class"
    This reverts commit 3115e37d486693fdc1f3ada9ebd560ef15daf209.

--- pages/common_page.js
+++
@@ -1,7 +0,0 @@
-class CommonPage{
-    constructor(page){
-        this.page = page
-        this.notification_banner = this.page.locator("#toast-container div div")
-    }
-}
-module.exports = { CommonPage }
