7e6443c4c20bfba6f7cf4680dacea9cf47486902
Pradyumna Galnimkar <pradyumna.galnimkar@gmail.com>
2025-01-03 22:27:39 +0530

    read excel file & returning data in list of objects method

--- utils/excelUtils.js
+++ utils/excelUtils.js
@@ -16,6 +16,29 @@ export default async function readExcel(file_path, search_text){
     return output
 }
 
+export async function readExcelFile(excelFilePath, sheetName){
+    let output = []
+    let headers = []
+    const workbook = new ExcelJs.Workbook();
+    await workbook.xlsx.readFile(excelFilePath);
+    const worksheet = workbook.getWorksheet(sheetName);
+    await worksheet.eachRow((row, rowNumber) => {
+        if(rowNumber === 1){
+            row.eachCell((cell, colNumber) => {
+                headers.push(cell.value);
+            })
+        }
+        else{
+            let rowData = {}
+            row.eachCell((cell, colNumber) => {
+                rowData[headers[colNumber-1]] = cell.value
+            })
+            output.push(rowData)
+        }
+    })
+    return output
+}
+
 export async function writeExcel(file_path, rowNumber, colNumber, updated_text){
     const workbook = new ExcelJs.Workbook();
     await workbook.xlsx.readFile(file_path);
