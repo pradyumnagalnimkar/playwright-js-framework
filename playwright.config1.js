6136bc5b5f4ebacf83a1aa557a90dfcf5656317c
Pradyumna Galnimkar <pradyumna.galnimkar@gmail.com>
2025-01-17 22:30:20 +0530

    configuring retries & workers

--- playwright.config1.js
+++ playwright.config1.js
@@ -18,9 +18,10 @@ module.exports = defineConfig({
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   // retries: process.env.CI ? 2 : 0,
-  retries: 1,
+  retries: 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  // workers: process.env.CI ? 3 : undefined,
+  workers: 4,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
