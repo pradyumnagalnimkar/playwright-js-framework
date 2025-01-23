20d0510fd75b6b1bda8caf7ce6d767ddc5898295
Pradyumna Galnimkar <pradyumna.galnimkar@gmail.com>
2025-01-23 23:23:48 +0530

    impelmented step definition for login to app

--- features/step_definitions/steps.js
+++ features/step_definitions/steps.js
@@ -1,13 +1,22 @@
-import { Given } from "@cucumber/cucumber";
+const { Given, When, Then } = require("@cucumber/cucumber");
+const { PageFactory } = require("../../pages/page_factory")
+const { playwright } = require("@playwright/test")
 
-Given('Login to application with {username} and {password}', function (username, password) {
+Given('Login to application with {username} and {password}', async function (username, password) {
     // Write code here that turns the phrase above into concrete actions
-    return 'pending';
+    const browser = await playwright.chromium.launch()
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    let page_factory = new PageFactory(page);
+    let login_page = page_factory.getLoginPage();
+    await login_page.loginToApp(username, password)
+    await expect(login_page.notification_banner).toContainText("Login Successfully")
+    await login_page.notification_banner.waitFor({state: "detached"})
   });
 
 When('Add {product_name} to cart', function (product_name) {
     // Write code here that turns the phrase above into concrete actions
-    return 'pending';
+    
 });
 
 Then('Verify {product_name} is displayed in cart', function (product_name) {
