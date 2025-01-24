a6afce02d650c5cb561d9c2930c97c71071ec52e
Pradyumna Galnimkar <pradyumna.galnimkar@gmail.com>
2025-01-25 00:06:15 +0530

    Implemented feature file steps E2E functionality

--- features/Ecommerce.feature
+++ features/Ecommerce.feature
@@ -2,6 +2,6 @@ Feature: Ecommerce Validations
     Scenario: Place Order
         Given Login to application with "ruben.amorim+admin@gmail.com" and "Test@12345678"
         When Add "qwerty" to cart
-        Then Verify "qwerty" is displayed in cart
+        Then Navigate to cart
         When Enter valid details and place Order
         Then Verify order is present in Orderhistory
--- features/step_definitions/steps.js
+++ features/step_definitions/steps.js
@@ -1,35 +1,54 @@
 const { Given, When, Then } = require("@cucumber/cucumber");
 const { PageFactory } = require("../../pages/page_factory")
-const { playwright } = require("@playwright/test")
+const { expect } = require("@playwright/test")
+const  playwright  = require("@playwright/test")
 
-Given('Login to application with {username} and {password}', async function (username, password) {
+Given('Login to application with {string} and {string}', {timeout: 10*1000}, async function (username, password) {
     // Write code here that turns the phrase above into concrete actions
-    const browser = await playwright.chromium.launch()
-    const context = await browser.newContext();
-    const page = await context.newPage();
-    let page_factory = new PageFactory(page);
-    let login_page = page_factory.getLoginPage();
+    const browser = await playwright.chromium.launch({headless: false})
+    const context = await browser.newContext()
+    this.page = await context.newPage();
+    this.page_factory = new PageFactory(this.page);
+    let login_page = this.page_factory.getLoginPage();
     await login_page.loginToApp(username, password)
     await expect(login_page.notification_banner).toContainText("Login Successfully")
     await login_page.notification_banner.waitFor({state: "detached"})
   });
 
-When('Add {product_name} to cart', function (product_name) {
+When('Add {string} to cart', {timeout: 10*1000}, async function (product_name) {
     // Write code here that turns the phrase above into concrete actions
+    this.dashboard_page = this.page_factory.getDashboardPage();
+    await this.dashboard_page.searchProductAddToCart(product_name);
+    await expect(this.dashboard_page.notification_banner).toContainText(" Product Added To Cart ")
     
 });
 
-Then('Verify {product_name} is displayed in cart', function (product_name) {
+Then('Navigate to cart', {timeout: 10*1000}, async function () {
     // Write code here that turns the phrase above into concrete actions
-    return 'pending';
+    await this.dashboard_page.navigateToCart()
 });
 
-When('Enter valid details and place Order', function () {
+When('Enter valid details and place Order', {timeout: 10*1000}, async function () {
     // Write code here that turns the phrase above into concrete actions
-    return 'pending';
+    let checkout_page = this.page_factory.getCheckoutPage()
+    await checkout_page.checkoutOrder('12','31', '123', "ruben.amorim+admin@gmail.com", "rahulshettyacademy")
+    await expect(checkout_page.applied_coupon_label).toHaveAttribute("style","color: green;")
+    await expect(checkout_page.shipping_address_label).toContainText("ruben.amorim+admin@gmail.com")
+    await checkout_page.selectCountry("Cuba")
+    await checkout_page.placeOrder()
+    this.order_details_page = this.page_factory.getOrderDetailsPage()
+    await expect(this.order_details_page.order_confirmation).toContainText(" Thankyou for the order. ")
+    this.order_id = await this.order_details_page.order_id_label.textContent()
+    console.log(`Order placed successfully! Order Id: ${this.order_id}`)
 });
 
-Then('Verify order is present in Orderhistory', function () {
+Then('Verify order is present in Orderhistory', async function () {
     // Write code here that turns the phrase above into concrete actions
-    return 'pending';
+    let order_list_page = this.page_factory.getOrderListPage()
+    await order_list_page.viewOrdeDetails(this.order_id)
+    await expect(this.page.getByText("Thank you for Shopping With Us")).toBeVisible()
+    await expect(this.order_details_page.order_summary_text).toContainText(" order summary ")
+    const actualOrderId = await this.order_details_page.order_id_confirmation_label.textContent()
+    expect(this.order_id.includes(actualOrderId)).toBeTruthy()
+    await expect(this.order_details_page.order_product_name).toContainText("qwerty")
   });
